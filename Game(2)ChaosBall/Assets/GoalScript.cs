5e22409a9740af795e04db56019f872d68ef6ff6
mottajacopo <mottajacopo@gmail.com>
2019-04-12 21:32:51 +0200

    add Game(2)ChaosBall

---
+++ Game(2)ChaosBall/Assets/GoalScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalScript : MonoBehaviour {
+
+	void OnTriggerEnter (Collider collider) {
+		GameObject collidedWith = collider.gameObject;
+		if (collidedWith.tag == gameObject.tag) {
+			GetComponent<Light>().intensity = 0;
+			Destroy(collidedWith);
+		}
+	}
+}
---
+++ Game(2)ChaosBall/Assets/VelocityScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VelocityScript : MonoBehaviour {
+	public float startSpeed = 50;
+
+	void Start () {
+		Rigidbody rigidBody = GetComponent<Rigidbody> ();
+		rigidBody.velocity = new Vector3(startSpeed, 0, startSpeed);
+	}
+}
