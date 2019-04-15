5a36cfd327e4a33ed78a44439dc4909a73ccdecb
mottajacopo <mottajacopo@gmail.com>
2019-04-15 14:23:42 +0200

    add Exercise_Motta_Salvo/Assets/MotionScript.cs and Exercise_Motta_Salvo/Assets/TriggerScript.cs

--- Exercise_Motta_Salvo/Assets/MotionScript.cs
+++ Exercise_Motta_Salvo/Assets/MotionScript.cs
@@ -1,21 +1,15 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class MotionScript : MonoBehaviour
 {
-
-    // Use this for initialization
-    void Start()
-    {
-
+    void Start() {
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float xMovement = Input.GetAxis("Horizontal") / 2;
-        float yMovement = Input.GetAxis("Vertical") / 2;
+    void Update() {
 
-        transform.Translate(xMovement, yMovement, 0);
+        float left_right = Input.GetAxis("Horizontal");
+        float up_down = Input.GetAxis("Vertical");
+
+        transform.Translate(left_right, up_down, 0);
     }
 }
--- Exercise_Motta_Salvo/Assets/TriggerScript.cs
+++ Exercise_Motta_Salvo/Assets/TriggerScript.cs
@@ -1,23 +1,22 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class TriggerScript : MonoBehaviour {
 
-	void Start () {	
+	void Start() {	
 	}
 
-	void Update () {	
+	void Update() {	
 	}
 	
-	void OnTriggerEnter (Collider other) {
+	void OnTriggerEnter(Collider other) {
 		print (other.gameObject.name + " has entered the " + this.gameObject.name);		
 	}
 	
-	void OnTriggerStay (Collider other) {
+	void OnTriggerStay(Collider other) {
 		print (other.gameObject.name + " is still in the " + this.gameObject.name);		
 	}
 	
-	void OnTriggerExit (Collider other) {
+	void OnTriggerExit(Collider other) {
 		print (other.gameObject.name + " has left the " + this.gameObject.name);		
 	}
 }
