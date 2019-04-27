dcd650ea59e10f98fdf1ff7cd80c3bacab2c1bb6
mottajacopo <mottajacopo@gmail.com>
2019-04-27 13:29:13 +0200

    modifica chaos ball

--- Game(2)ChaosBall/Assets/GameControlScript.cs
+++ Game(2)ChaosBall/Assets/GameControlScript.cs
@@ -1,21 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class GameControlScript : MonoBehaviour {
 	public GameObject blueBall, greenBall, redBall, orangeBall;
-	private bool isGameOver = true;	
+	private bool isGameOver = true;
+    private bool restart = false;
+    public GameObject player;
+    private float elapsedTime = 0;
 
-	void Update () {
-		isGameOver = !blueBall && !greenBall && !redBall && !orangeBall;		
+
+    void Update () {
+		isGameOver = !blueBall && !greenBall && !redBall && !orangeBall;
+        if (!isGameOver)
+        {
+            elapsedTime += Time.deltaTime;
+        }
+        
+        if (restart)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            elapsedTime = 0;
+        }
 	}
 
-	void OnGUI() {
-		if(isGameOver) {
-			Rect rect = new Rect (Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 75);
-			GUI.Box (rect, "Game Over");
 
-			Rect rect2 = new Rect (Screen.width / 2 - 30, Screen.height / 2 - 25, 60, 50);
-			GUI.Label (rect2, "Good Job!");
-		}
+    void OnGUI() {
+		if(isGameOver) {
+			Rect rect = new Rect (Screen.width / 2 -75, Screen.height / 2, 150, 40);
+			GUI.Box (rect, "Game Over  Good Job!");
+
+            GUI.Box(new Rect(Screen.width / 2-75 , Screen.height / 2 + 50, 150, 30), "Your Time Was " + ((int)elapsedTime).ToString());
+
+            string message = "Click to Play Again";
+
+            Rect startButton = new Rect(Screen.width / 2-75 , Screen.height / 2 + 90, 150, 30);
+            if (GUI.Button(startButton, message))
+            {
+                restart = true;
+            }
+        }
 	}
 }
